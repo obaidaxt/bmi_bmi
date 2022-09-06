3c42b772698e03b01eaa14d34804ba27b6da9aaa
Obaida Aljjarrah <Obaidaxt@gmail.com>
2022-09-06 16:50:49 +0200

    1650

---
+++ lib/views/screen1/screen1.dart
@@ -0,0 +1,216 @@
+import 'package:bmi_bmi/models/bmi.dart';
+import 'package:bmi_bmi/views/screen2/screen2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Screen1 extends StatefulWidget {
+  const Screen1({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Screen1> createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    BodyMassindex bmi = BodyMassindex();
+    // bmi.gewicht = _gewicht;
+
+    double _bmi = bmi.gewicht / (bmi.groesse * bmi.groesse / 10000);
+    String bmass = _bmi.toStringAsFixed(2);
+    double _sliderBmi;
+    if (_bmi.isNaN || _bmi.isInfinite) {
+      _bmi = 0;
+    }
+    if (_bmi > 40) {
+      _sliderBmi = 40;
+    } else if (_bmi < 8) {
+      _sliderBmi = 8;
+    } else {
+      _sliderBmi = _bmi;
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          // leading: IconButton(
+          //     onPressed: () {
+          //       Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => const Screen2()),
+          //       );
+          //     },
+          //     icon: Icon(Icons.arrow_back)),
+
+          leading: TextButton(
+              style: TextButton.styleFrom(padding: EdgeInsets.only(top: 5)),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Screen2(title: 'Page 1'),
+                    ));
+              },
+              child: Text(
+                'Zurück',
+                style: TextStyle(color: Colors.black),
+              )),
+          title: Text('ideal Gewicht'),
+        ),
+        body: Container(
+          color: Color.fromARGB(255, 194, 165, 206),
+          child: Column(
+            children: <Widget>[
+              Container(),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 200, 10, 50),
+                padding: EdgeInsets.all(5),
+                color: Colors.green,
+                child: Container(
+                  width: double.infinity,
+                  height: 55,
+                  color: Colors.white70,
+                  child: Center(
+                    child: TextFormField(
+                      initialValue: '',
+                      decoration: InputDecoration(labelText: 'Gewicht in Kg'),
+                      textAlign: TextAlign.center,
+                      onChanged: (value) {
+                        final gewicht = double.tryParse(value);
+                        if (gewicht != null) {
+                          setState(() {
+                            bmi.gewicht = gewicht;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                color: Colors.green,
+                child: Container(
+                  width: double.infinity,
+                  height: 55,
+                  color: Colors.white70,
+                  child: Center(
+                    child: TextFormField(
+                      initialValue: '',
+                      decoration: InputDecoration(labelText: 'Größe in cm'),
+                      textAlign: TextAlign.center,
+                      onChanged: (value) {
+                        var groesse = double.tryParse(value);
+                        if (groesse != null) {
+                          setState(() {
+                            bmi.groesse = groesse;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              // Container(
+              // margin: EdgeInsets.all(90),
+              // padding: EdgeInsets.all(50),
+              // color: Colors.green,
+              // Container(
+              //   width: double.infinity,
+              //   height: 55,
+              //   color: _bmi > 25 || _bmi < 19 ? Colors.red : Colors.green,
+              //   child: Text(_bmi.toStringAsFixed(0)),
+              // ),
+              // ),
+
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                child: Stack(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 10,
+                          child: Container(
+                            height: 50,
+                            color: Color.fromARGB(255, 21, 132, 223),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 7,
+                          child: Container(
+                            height: 50,
+                            color: Colors.green,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 15,
+                          child: Container(
+                            height: 50,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      value: _sliderBmi,
+                      max: 40,
+                      min: 8,
+                      // label: _bmi.round().toString(),
+                      onChanged: (double value) {
+                        // setState(() {
+                        //   _bmi = value;
+                        // });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              // Container(
+              //   color: Colors.white,
+              //   child: TextButton(
+              //     style: TextButton.styleFrom(
+              //       padding: const EdgeInsets.all(16.0),
+              //       textStyle: const TextStyle(fontSize: 20),
+              //     ),
+              //     onPressed: () {},
+              //     child: const Text('Berechnen'),
+              //   ),
+              // ),
+              Container(
+                width: double.infinity,
+                child: Text(
+                  "Screen1 dein Bmi ist:",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+
+              Container(
+                width: double.infinity,
+                child: Text(
+                  "$bmass",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
